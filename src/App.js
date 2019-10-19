2ca74dfa417aa7b946dfb3c5b7be47104bcb404d
Temiloluwa Ojo <themmyloluwaa@gmail.com>
2019-10-19 05:16:05 +0100

    working on location component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Footer from './components/footer/footer'
 import Header from './components/header/header'
 import Tips from './components/tips/tips'
+import Location from './components/location/location'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Tips />
+      <Location />
       <Footer />
     </div>
   );
---
+++ src/components/location/location.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Location = () => {
+    return (
+        <section className="card section col-lg-6 float-right">
+            <h3>Welcome dear one</h3>
+            <p>Share <i className="fas fa-street-view"></i></p>
+        </section>
+    )
+
+}
+
+export default Location;
